dbdf54256b56fe33ae104147abc2fb89402bca30
CaIraith <hm.leewenxin@gmail.com>
2026-03-12 21:46:42 +0800

    Update EditCommand.java

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -90,9 +90,8 @@ public class EditCommand extends Command {
             throw new CommandException(e.getMessage());
         }
 
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        model.setPerson(personToEdit, editedPerson);
         model.resetFilteredPersonList();
+        model.setPerson(personToEdit, editedPerson);
         return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson)));
     }
 
