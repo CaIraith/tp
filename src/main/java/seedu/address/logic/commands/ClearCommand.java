c1bcd10a0efd053189f2ed308cf117e598a28b53
CaIraith <hm.leewenxin@gmail.com>
2026-04-06 13:15:27 +0800

    Fix ClearCommand undo filter bug

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
@@ -16,11 +17,13 @@ public class ClearCommand extends UndoableCommand {
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
     private ArrayList<Person> previousPersonList;
+    private Predicate<? super Person> previousPredicate;
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         previousPersonList = new ArrayList<>(model.getAddressBook().getPersonList());
+        previousPredicate = model.getFilteredPersonPredicate();
         model.setAddressBook(new AddressBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
@@ -30,6 +33,7 @@ public class ClearCommand extends UndoableCommand {
         for (Person person : previousPersonList) {
             model.addPerson(person);
         }
+        model.setFilteredPersonPredicate(previousPredicate);
     }
 
     @Override
